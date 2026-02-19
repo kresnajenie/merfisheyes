a40122715c45d2d15711fa784795242fe6be5705
Dwagon-81 <82969298+Dwagon-81@users.noreply.github.com>
2026-02-18 23:13:39 -0800

    Hopefuly fixed navbar upload and save button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -99,7 +99,7 @@ export const Navbar = ({ onUploadClick }: NavbarProps) => {
         )}
 
         {/* Show Upload & Save button on /sm-viewer when dataset is loaded */}
-        {pathname === "/sm-viewer" && onUploadClick && (
+        {pathname?.startsWith("/sm-viewer") && onUploadClick && (
           <NavbarItem>
             <Button color="primary" onPress={onUploadClick}>
               Upload & Save
